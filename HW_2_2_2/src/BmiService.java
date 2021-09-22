3b5dea2e0a32b0c973c768fc420025ea2449e522
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-09-22 14:51:35 +0300

    Add changes

--- HW_2_2_2/src/BmiService.java
+++ HW_2_2_2/src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     // height в см
-    public float calculate(float height, int weight){
-        float imt = weight/((height*height)/10000);
+    public float calculate(float height, int weight) {
+        float imt = weight / ((height * height) / 10000);
         return imt;
     }
 }
--- HW_2_2_2/src/Main.java
+++ HW_2_2_2/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float imt = service.calculate(178,85);
+        float imt = service.calculate(178, 85);
         //height в см
         System.out.println(imt);
 
